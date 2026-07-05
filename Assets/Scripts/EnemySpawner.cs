ae6ea1642c0ddec80bfa32c06aeb5f080d66c481
NeemaDev <brueggemann.ines@googlemail.com>
2026-07-05 14:59:31 +0200

    Added enemy spawner.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    public GameObject enemyPrefab;
+    public int enemiesToSpawn = 5;
+
+    [Header("Spawn Locations")]
+    public Vector2[] spawns;
+
+    private int spawnedEnemies = 0;
+    private int attempts = 0;
+    private int maxAttempts = 1000;
+
+    private void Start()
+    {
+        SpawnEnemies();
+    }
+
+    private void SpawnEnemies()
+    {
+        if(spawns == null || spawns.Length == 0)
+        {
+            throw new System.NullReferenceException("No spawn points defined.");
+        }
+
+        while(spawnedEnemies < enemiesToSpawn && attempts <= maxAttempts)
+        {
+            int randomIndex = Random.Range(0,spawns.Length);        
+            Vector2 spawnLocation = spawns[randomIndex];
+            
+            Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+            spawnedEnemies++;
+            Debug.Log($"Enemy spawned at {spawnLocation.x}, {spawnLocation.y}");
+
+            attempts++;
+        }
+
+        if(attempts >= maxAttempts)
+        {
+            Debug.Log("Max enemy spawning attempts reached.");
+        }
+    }
+}
