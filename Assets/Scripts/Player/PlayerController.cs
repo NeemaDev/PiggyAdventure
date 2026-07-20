552a6c857c2a8a7e4b86ac7328f947a6f3eb637c
NeemaDev <brueggemann.ines@googlemail.com>
2026-07-20 18:13:27 +0200

    Added quicksand gameobject & implemented drag on player.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,16 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float quicksandDragFactor = 0.5f;
 
+    private Rigidbody2D rb;
     private Vector2 currentMoveInput = Vector2.zero;
     private Vector3 targetPosition;
     private bool isMoving = false;
+    private Transform quicksandCenter;
 
     void Start()
     {
-
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -26,18 +28,26 @@ public class PlayerController : MonoBehaviour
         currentMoveInput = value.Get<Vector2>();
     }
 
-    public void OnClick(InputValue value)
-    {
-        Vector2 mousePosition = Mouse.current.position.value;
-        targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        targetPosition.z = transform.position.z;
-
-        isMoving = true;
-    }
-
     private void MovePlayer()
     {
-        var movementDistance = moveSpeed * Time.deltaTime;
+        var effectiveSpeed = moveSpeed;
+
+        if (quicksandCenter != null && currentMoveInput != Vector2.zero)
+        {
+            Vector2 inputDirection = currentMoveInput.normalized;
+            Vector2 directionToQuicksandCenter = (quicksandCenter.position - transform.position).normalized;
+
+            float dotProduct = Vector2.Dot(inputDirection, directionToQuicksandCenter);
+
+            if (dotProduct < 0)
+            {
+                // Using dotProduct to influence the speed factor instantly. 
+                // dot = -1 --> hardest drag (0.2), dot = 0.5 --> slight drag at 0.6 (60%) speed.
+                effectiveSpeed *= Mathf.Lerp(1f, quicksandDragFactor, -dotProduct);
+            }
+        }
+
+        var movementDistance = effectiveSpeed * Time.deltaTime;
 
         if (currentMoveInput != Vector2.zero)
         {
@@ -54,6 +64,25 @@ public class PlayerController : MonoBehaviour
             {
                 transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementDistance);
             }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("QuickSand"))
+        {
+            // Assign quicksand center to trigger movement adjustment in quicksand.
+            quicksandCenter = collision.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("QuickSand"))
+        {
+            // Resetting linear velocity after leaving quicksand 
+            // (it stays on the last set value and continues to drag the player).
+            rb.linearVelocity = Vector2.zero;
         }
     }
 }
