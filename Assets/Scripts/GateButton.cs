e5d0a4951eb8d1ccb55c9eb1592899efd6727e33
NeemaDev <brueggemann.ines@googlemail.com>
2026-09-19 10:20:43 +0200

    Added switch of sprite for Castegate triggers.

--- Assets/Scripts/GateButton.cs
+++ Assets/Scripts/GateButton.cs
@@ -5,27 +5,21 @@ public class GateButton : MonoBehaviour
 {
     [Header("Puzzle Settings")]
     [SerializeField] private string buttonId;
-     private bool state = false;
-     private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite inactiveSprite;
+    [SerializeField] private Sprite activeSprite;
+    private bool state = false;
+    private SpriteRenderer spriteRenderer;
 
-    public static event Action<string,bool> OnButtonStateChanged;
+    public static event Action<string, bool> OnButtonStateChanged;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.GetComponent<PlayerController>() != null)
+        if (collision.GetComponent<PlayerController>() != null)
         {
             state = !state;
             OnButtonStateChanged?.Invoke(buttonId, state);
 
-            // Change Color.
-            if (state)
-            {
-                ChangeColor(Color.green);
-            }
-            else
-            {
-                ChangeColor(Color.red);
-            }
+            ChangeSprite();
         }
     }
 
@@ -34,11 +28,15 @@ public class GateButton : MonoBehaviour
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
-    private void ChangeColor(Color newColor)
+    private void ChangeSprite()
     {
-        if(spriteRenderer != null)
+        if (state)
         {
-            spriteRenderer.color = newColor;
+            spriteRenderer.sprite = activeSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = inactiveSprite;
         }
     }
 }
